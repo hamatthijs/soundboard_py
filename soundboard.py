0fcd55016e4225ca305a1284dfe8f8f1b81cf792
hamatthijs <matthijsrenzenbrink@gmail.com>
2024-05-10 06:04:53 +0200

    feat: :sparkles: add upload functionality

--- soundboard.py
+++ soundboard.py
@@ -3,7 +3,7 @@ from pathlib import Path
 root = Path(__file__).parent
 
 def command():
-    text = input("command: ").split()
+    text = input("command: ").split(" ", 1)
     print(text)
     if len(text) == 0:
         command()
@@ -14,9 +14,14 @@ def command():
             exit()
         elif text[0] == "play":
             if len(text) == 2:
-                play(str(text[1]))
+                play(text[1])
             else:
                 incorrect_usage("play")
+        elif text[0] == "upload":
+            if len(text) == 2:
+                upload(text[1])
+            else:
+                incorrect_usage("upload")
 
 def unknown():
     print("Unknown command")
@@ -35,17 +40,26 @@ def intro():
     help()
 
 def play(file):
+    filename = str(file).rsplit(".",1)[0]
+    print(f"Playing {filename}")
     file = root/"files"/file
     os.startfile(file)
     command()
 
-
-def incorrect_usage(user):
-    print(f"Incorrect usage of {user}")
-    if user == "play":
-        print("Usage: play <sound>")
+def upload(file):
+    print("Uploading a sound")
+    destination = root/"files"
+    os.system(f"copy {file} {destination}")
     command()
 
+
+def incorrect_usage(passed_command):
+    print(f"Incorrect usage of {passed_command}")
+    if passed_command == "play":
+        print("Usage: play <sound>")
+    elif passed_command == "upload":
+        print("Usage: upload <sound>")
+    command()
 
 
 intro()
